0bdb83afafc9487d16fef19abc819b78bb77b8fe
Emiliano Borghi <emiliano.borghi@gmail.com>
2021-09-08 20:57:17 -0300

    Use smart pointers instead of raw pointers

--- src/GaitCtrller.cpp
+++ src/GaitCtrller.cpp
@@ -1,21 +1,20 @@
 #include "GaitCtrller.h"
 
-GaitCtrller::GaitCtrller(double freq, double* PIDParam) {
+GaitCtrller::GaitCtrller(double freq, double* PIDParam)
+  : _quadruped{ buildMiniCheetah<float>() }
+  , _model{ _quadruped.buildModel() }
+  , convexMPC{ std::make_unique<ConvexMPCLocomotion>(1.0 / freq, 13) }
+  , _legController{ std::make_unique<LegController<float>>(_quadruped) }
+  , _stateEstimator{ std::make_unique<StateEstimatorContainer<float>>(
+        cheaterState.get(), &_vectorNavData, _legController->datas, &_stateEstimate,
+        controlParameters.get()) }
+  , _desiredStateCommand{ std::make_unique<DesiredStateCommand<float>>(1.0 / freq) }
+  , safetyChecker{ std::make_unique<SafetyChecker<float>>() }
+{
   for (int i = 0; i < 4; i++) {
     ctrlParam(i) = PIDParam[i];
   }
   _gamepadCommand.resize(4);
-  FloatingBaseModel<float> _model;
-  convexMPC = new ConvexMPCLocomotion(1.0 / freq, 13);
-
-  _quadruped = buildMiniCheetah<float>();
-  _model = _quadruped.buildModel();
-
-  _legController = new LegController<float>(_quadruped);
-
-  _stateEstimator = new StateEstimatorContainer<float>(
-      cheaterState, &_vectorNavData, _legController->datas, &_stateEstimate,
-      controlParameters);
 
   // reset the state estimator
   _stateEstimator->removeAllEstimators();
@@ -24,31 +23,13 @@ GaitCtrller::GaitCtrller(double freq, double* PIDParam) {
   contactDefault << 0.5, 0.5, 0.5, 0.5;
   _stateEstimator->setContactPhase(contactDefault);
 
-  //源代码中中对姿态和位置速度的估计，添加到状态估计器中
   _stateEstimator->addEstimator<VectorNavOrientationEstimator<float>>();
   _stateEstimator->addEstimator<LinearKFPositionVelocityEstimator<float>>();
 
-  _desiredStateCommand = new DesiredStateCommand<float>(1.0 / freq);
-
-  safetyChecker = new SafetyChecker<float>();
-
-  // for (int i = 0; i < 100;
-  //      i++) {  // init state estimator data because of the filter
-  //   _stateEstimator->run();
-
-  //   // Update the data from the robot
-  //   _legController->updateData(&_legdata);
-  // }
   std::cout << "finish init controller" << std::endl;
 }
 
-GaitCtrller::~GaitCtrller() {
-  delete convexMPC;
-  delete _legController;
-  delete _stateEstimator;
-  delete _desiredStateCommand;
-  delete safetyChecker;
-}
+GaitCtrller::~GaitCtrller() {}
 
 void GaitCtrller::SetIMUData(double* imuData) {
   _vectorNavData.accelerometer(0, 0) = imuData[0];
@@ -109,45 +90,11 @@ void GaitCtrller::SetRobotVel(double* vel) {
   } else {
   _gamepadCommand[2] = vel[2] * 1.0;
   }
-  // std::cout << "set vel to: " << _gamepadCommand[0] << " " << _gamepadCommand[1]
-  //           << " " << _gamepadCommand[2] << std::endl;
 }
 
-void GaitCtrller::ToqueCalculator(double* imuData, double* motorData,
-                                  double* effort) {
-  // std::vector<double> effort;
-  // double effort[12];
-  // set imu data
-  _vectorNavData.accelerometer(0, 0) = imuData[0];
-  _vectorNavData.accelerometer(1, 0) = imuData[1];
-  _vectorNavData.accelerometer(2, 0) = imuData[2];
-  _vectorNavData.quat(0, 0) = imuData[3];
-  _vectorNavData.quat(1, 0) = imuData[4];
-  _vectorNavData.quat(2, 0) = imuData[5];
-  _vectorNavData.quat(3, 0) = imuData[6];
-  _vectorNavData.gyro(0, 0) = imuData[7];
-  _vectorNavData.gyro(1, 0) = imuData[8];
-  _vectorNavData.gyro(2, 0) = imuData[9];
-  // std::cout << "imu = " << std::endl;
-  // for (int i = 0; i < 10; i++) {
-  //   std::cout << imuData[i] << " ";
-  // }
-  // std::cout << std::endl;
-  // set motor data
-  for (int i = 0; i < 4; i++) {
-    _legdata.q_abad[i] = motorData[i * 3];
-    _legdata.q_hip[i] = motorData[i * 3 + 1];
-    _legdata.q_knee[i] = motorData[i * 3 + 2];
-    _legdata.qd_abad[i] = motorData[12 + i * 3];
-    _legdata.qd_hip[i] = motorData[12 + i * 3 + 1];
-    _legdata.qd_knee[i] = motorData[12 + i * 3 + 2];
-  }
-
-  // state estimator
-  _stateEstimator->run();
-
-  // Update the data from the robot
-  _legController->updateData(&_legdata);
+void GaitCtrller::TorqueCalculator(double* imuData, double* motorData,
+                                   double* effort) {
+  PreWork(imuData, motorData);
 
   // Setup the leg controller for a new iteration
   _legController->zeroCommand();
@@ -160,19 +107,19 @@ void GaitCtrller::ToqueCalculator(double* imuData, double* motorData,
   //safety check
   if(!safetyChecker->checkSafeOrientation(*_stateEstimator)){
     _safetyCheck = false;
-    std::cout << "broken: Orientation Safety Ceck FAIL" << std::endl;
+    std::cout << "broken: Orientation Safety Check FAIL" << std::endl;
 
   }else if (!safetyChecker->checkPDesFoot(_quadruped, *_legController)) {
     _safetyCheck = false;
-    std::cout << "broken: Foot Position Safety Ceck FAIL" << std::endl;
+    std::cout << "broken: Foot Position Safety Check FAIL" << std::endl;
 
   }else if (!safetyChecker->checkForceFeedForward(*_legController)) {
     _safetyCheck = false;
-    std::cout << "broken: Force FeedForward Safety Ceck FAIL" << std::endl;
+    std::cout << "broken: Force FeedForward Safety Check FAIL" << std::endl;
 
   }else if (!safetyChecker->checkJointLimit(*_legController)) {
     _safetyCheck = false;
-    std::cout << "broken: Joint Limit Safety Ceck FAIL" << std::endl;
+    std::cout << "broken: Joint Limit Safety Check FAIL" << std::endl;
   }
 
   convexMPC->run(_quadruped, *_legController, *_stateEstimator,
@@ -180,21 +127,19 @@ void GaitCtrller::ToqueCalculator(double* imuData, double* motorData,
 
   _legController->updateCommand(&legcommand, ctrlParam);
 
-  // effort.resize(12);
-  if(_safetyCheck){
+  if(_safetyCheck) {
     for (int i = 0; i < 4; i++) {
       effort[i * 3] = legcommand.tau_abad_ff[i];
       effort[i * 3 + 1] = legcommand.tau_hip_ff[i];
       effort[i * 3 + 2] = legcommand.tau_knee_ff[i];
     }
-  }else{
+  } else {
     for (int i = 0; i < 4; i++) {
       effort[i * 3] = 0.0;
       effort[i * 3 + 1] = 0.0;
       effort[i * 3 + 2] = 0.0;
     }
   }
-  
 
   // return effort;
 }
